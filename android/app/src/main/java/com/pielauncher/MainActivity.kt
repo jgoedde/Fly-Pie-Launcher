9d73ae9bdcc0fb7f133445d19533783114a1b462
Julian Gödde <129423545+jgoedde@users.noreply.github.com>
2025-03-25 08:58:12 +0100

    Try fix wallpaper

--- android/app/src/main/java/com/pielauncher/MainActivity.kt
+++ android/app/src/main/java/com/pielauncher/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.pielauncher
 
+import android.os.Bundle
+import android.view.WindowManager
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
-
+  
+  override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER)
+  }
+  
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
