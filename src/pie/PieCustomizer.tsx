36664f6507ea1caa23610541065c0e46aaf10c70
Julian Gödde <goedde@tuta.io>
2025-03-29 09:58:49 +0100

    feat(config): Add layer validation feedback

--- src/pie/PieCustomizer.tsx
+++ src/pie/PieCustomizer.tsx
@@ -1,4 +1,5 @@
 import {
+    Alert,
     BackHandler,
     ScrollView,
     StyleSheet,
@@ -7,9 +8,9 @@ import {
     View,
 } from 'react-native';
 import React, { useCallback, useEffect, useState } from 'react';
-import { LayersSchema, useLayerConfig } from './use-layer-config.ts';
-import { useInstalledApps } from './use-installed-apps.ts';
-import { ZodError } from 'zod';
+import { LayerSchema, useLayerConfig } from './layers/use-layer-config.ts';
+import { useInstalledApps } from '../installed-apps/use-installed-apps.ts';
+import { z, ZodError } from 'zod';
 
 export default function PieCustomizer({ exit }: { exit: () => void }) {
     const { apps } = useInstalledApps();
@@ -22,19 +23,45 @@ export default function PieCustomizer({ exit }: { exit: () => void }) {
         try {
             const parsedLayers = JSON.parse(layersStr);
 
-            const parse = LayersSchema.parse(parsedLayers);
+            const parse = z
+                .array(LayerSchema)
+                .min(1, { message: 'At least one layer must be defined.' })
+                .refine(it => it.filter(l => l.isBaseLayer).length === 1, {
+                    message: 'There must be a maximum of one base layer',
+                })
+                .parse(parsedLayers);
 
             setLayers(parse);
         } catch (e) {
             if (e instanceof ZodError) {
-                console.error(e.toString());
+                displayZodErrors(e);
             } else if (e instanceof Error) {
-                console.error(e.message);
+                Alert.alert(
+                    'Layers',
+                    'An unknown error occurred.\n' + e.message,
+                );
             }
             setLayersStr(JSON.stringify(layers, undefined, 2));
         }
     }, [layers, layersStr, setLayers]);
 
+    const displayZodErrors = (error: ZodError) => {
+        if (!error || !error.errors) {
+            return;
+        }
+
+        // Format error messages
+        const errorMessages = error.errors.map(err => {
+            const fieldPath =
+                err.path.length > 0 ? err.path.join(' → ') : 'General Error'; // Handle global errors
+            return `${fieldPath}: ${err.message}`;
+        });
+
+        // Show Alert dialog
+        Alert.alert('Validation Errors', errorMessages.join('\n\n'), [
+            { text: 'OK' },
+        ]);
+    };
     useEffect(() => {
         const backHandler = BackHandler.addEventListener(
             'hardwareBackPress',
