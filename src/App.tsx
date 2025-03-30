89b3ac20ffdab4e9ad0a4a0f65ee1587ee44b12f
Julian Gödde <goedde@tuta.io>
2025-03-30 09:20:14 +0200

    0.0.6

--- src/App.tsx
+++ src/App.tsx
@@ -104,7 +104,7 @@ export default function App() {
         setCurrentLayerId(layers.find(l => l.isBaseLayer)?.id ?? 1);
         setCurrentTouchPoint(undefined);
         setCenter(undefined);
-        setSelectedShortcut(undefined)
+        setSelectedShortcut(undefined);
         setShortcutDropdownAnchor(undefined);
         setCurrentDropDownTouchPoint(undefined);
     }, [layers]);
